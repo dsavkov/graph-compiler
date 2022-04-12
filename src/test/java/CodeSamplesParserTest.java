7c2a644375064775d0e8d7949264a1e14b8140be
Danila Savkov <d.savkov@syberry.com>
2022-04-12 21:35:06 +0300

    Update CodeSamplesParserTest

--- src/test/java/CodeSamplesParserTest.java
+++ src/test/java/CodeSamplesParserTest.java
@@ -16,16 +16,20 @@ import java.util.Objects;
 public class CodeSamplesParserTest {
 
 	private static final File RESOURCES_FOLDER = Paths.get("src", "test", "resources").toFile();
-	private static List<String> exampleFiles;
+	private static final List<String> EXAMPLE_FILES = new ArrayList<>();
 
 	@BeforeAll
 	static void beforeAll() {
-		exampleFiles = getExampleFiles();
+		for (File file : Objects.requireNonNull(RESOURCES_FOLDER.listFiles())) {
+			if (!file.isDirectory()) {
+				EXAMPLE_FILES.add(file.getAbsolutePath());
+			}
+		}
 	}
 
 	@Test
 	void testSample1() throws IOException {
-		var inputStream = new ANTLRFileStream(exampleFiles.get(0));
+		var inputStream = new ANTLRFileStream(EXAMPLE_FILES.get(0));
 		var lexer = new GraphLexer(inputStream);
 		var parser = new GraphParser(new CommonTokenStream(lexer));
 		var tree = parser.compilationUnit();
@@ -35,7 +39,7 @@ public class CodeSamplesParserTest {
 
 	@Test
 	void testSample2() throws IOException {
-		var inputStream = new ANTLRFileStream(exampleFiles.get(1));
+		var inputStream = new ANTLRFileStream(EXAMPLE_FILES.get(1));
 		var lexer = new GraphLexer(inputStream);
 		var parser = new GraphParser(new CommonTokenStream(lexer));
 		var tree = parser.compilationUnit();
@@ -45,21 +49,11 @@ public class CodeSamplesParserTest {
 
 	@Test
 	void testSample3() throws IOException {
-		var inputStream = new ANTLRFileStream(exampleFiles.get(2));
+		var inputStream = new ANTLRFileStream(EXAMPLE_FILES.get(2));
 		var lexer = new GraphLexer(inputStream);
 		var parser = new GraphParser(new CommonTokenStream(lexer));
 		var tree = parser.compilationUnit();
 		var visitor = new GraphCustomVisitor();
 		var output = visitor.visit(tree);
-	}
-
-	private static List<String> getExampleFiles() {
-		List<String> result = new ArrayList<>();
-		for (File file : Objects.requireNonNull(RESOURCES_FOLDER.listFiles())) {
-			if (!file.isDirectory()) {
-				result.add(file.getAbsolutePath());
-			}
-		}
-		return result;
 	}
 }
